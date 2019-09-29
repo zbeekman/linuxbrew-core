46a44eca12f4bad6ba3908fad824c469983cfb0b
Michka Popoff <michkapopoff@gmail.com>
2019-09-29 21:27:17 +0200

    print content

--- .github/workflows/azure/check.py
+++ .github/workflows/azure/check.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+import json
+
 if __name__ == "__main__":
-    print("Hello world!")
+    event_path = os.environ["GITHUB_EVENT_PATH"]
+    event_data = json.load(open(event_path))
+
+    check_run = event_data["check_run"]
+
+    print(check_run["status"])
+    print(check_run["name"])
+    sys.exit(78)
